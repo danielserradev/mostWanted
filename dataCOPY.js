3ea46751f00513b783901a96afb2aea17d027598
trevor clements <trevortheamazing@gmail.com>
2019-09-19 10:04:06 -0500

    added new file - dataCOPY.js.  edited to only hold 2 objects.

---
+++ dataCOPY.js
@@ -0,0 +1,29 @@
+// You may modify this for testing purposes.
+const data = [
+    {
+        "id": 272822514,
+        "firstName": "Billy",
+        "lastName": "Bob",
+        "gender": "male",
+        "dob": "1/18/1949",
+        "height": 71,
+        "weight": 175,
+        "eyeColor": "brown",
+        "occupation": "programmer",
+        "parents": [],
+        "currentSpouse": 401222887
+    },
+    {
+        "id": 401222887,
+        "firstName": "Uma",
+        "lastName": "Bob",
+        "gender": "female",
+        "dob": "4/1/1947",
+        "height": 65,
+        "weight": 162,
+        "eyeColor": "brown",
+        "occupation": "assistant",
+        "parents": [],
+        "currentSpouse": 272822514
+    }
+];
